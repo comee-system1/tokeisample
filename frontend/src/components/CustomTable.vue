c4dc44a5714ca10407ead1edea22cd793d716bdf
comee-system1 <system@comee.jp>
2022-03-30 13:54:27 +0900

    カスタムテーブル

---
+++ frontend/src/components/CustomTable.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container-fluid">
+        <wj-flex-grid :itemsSource='data'>
+          <wj-flex-grid-column binding="country" header="国"></wj-flex-grid-column>
+          <wj-flex-grid-column binding="downloads" header="DL数"></wj-flex-grid-column>
+          <wj-flex-grid-column binding="sales" header="売上"></wj-flex-grid-column>
+          <wj-flex-grid-column binding="expenses" header="費用"></wj-flex-grid-column>
+        </wj-flex-grid>
+    </div>
+</template>
+
+<script>
+
+import "@grapecity/wijmo.styles/wijmo.css";
+import '@grapecity/wijmo.vue2.grid';
+import '@grapecity/wijmo.vue2.grid.filter';
+import * as wjCore from "@grapecity/wijmo";
+import { getData } from "../services/data.custom";
+import * as gridFilter from '@grapecity/wijmo.grid.filter';
+
+export default {
+  data: function() {
+    return {
+      data: getData()
+    };
+  },
+  created: function() {
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import MainWorld from './components/MainWorld.vue'
 import MainAdmin from './components/MainAdmin.vue'
 import HttpRequest from './components/HttpRequest.vue'
+import Custom from './components/CustomTable.vue'
 
 Vue.use(Router)
 
@@ -11,6 +12,7 @@ export default new Router({
   routes: [
     { path: '/mainadmin', name:MainAdmin, component: MainAdmin},
     { path: '/mainworld', name:MainWorld, component: MainWorld},
-    { path: '/httprequest', name:HttpRequest, component: HttpRequest}
+    { path: '/httprequest', name:HttpRequest, component: HttpRequest},
+    { path: '/custom', name:Custom, component: Custom},
   ]
 })
